54df8bf6e56cae30eecef3b0334a14753a3ebbc6
lynettesun <shuoyue.sun13@alumni.xjtlu.edu.cn>
2019-12-16 16:51:02 +0800

    adaboosting

--- adaboosting_tree.py
+++ adaboosting_tree.py
@@ -32,18 +32,18 @@ X_train, y_train, X_test, y_test = train_test_split(X, y)
 # train
 number_of_classifiers = 5
 
-n_samples, n_features = np.shape(X)
+n_samples, n_features = np.shape(X_train)
 w = np.full(n_samples, (1 / n_samples))
 clfs = []
 for _ in range(number_of_classifiers):
     min_error = float('inf')
     for feature_i in range(n_features):
-        unique_values = np.unique(X[:, feature_i])
+        unique_values = np.unique(X_train[:, feature_i])
         for value in unique_values:
             p = 1
-            prediction = np.ones(np.shape(y))
-            prediction[X[:, feature_i] < value] = -1
-            error = sum(w[y != prediction])
+            prediction = np.ones(np.shape(y_train))
+            prediction[X_train[:, feature_i] < value] = -1
+            error = sum(w[y_train != prediction])
             if error > 0.5:
                 error = 1 - error
                 p = -1
@@ -53,14 +53,24 @@ for _ in range(number_of_classifiers):
                 feature_index = feature_i
                 min_error = error
     alpha = 0.5 * np.log((1.0 - min_error) / (min_error + 1e-10))
-    predictions = np.ones(np.shape(y))
-    negative_idx = (polarity * X[:, feature_index] < polarity * threshold)
+    predictions = np.ones(np.shape(y_train))
+    negative_idx = (polarity * X_train[:, feature_index] < polarity * threshold)
     predictions[negative_idx] = -1
-    w *= np.exp(-alpha * y * predictions)
+    w *= np.exp(-alpha * y_train * predictions)
     w /= np.sum(w)
     
-    # Save classifier
-    clfs.append({'feature': feature_index, 'threshold': threshold, 'polarity': polarity, 'alpha': alpha})
+    clfs.append({'feature_index': feature_index, 'threshold': threshold, 'polarity': polarity, 'alpha': alpha})
 
+# test
+n_samples = np.shape(X_test)[0]
+y_pred = np.zeros((n_samples, 1))
+for clf in clfs:
+    predictions = np.ones(np.shape(y_pred))
+    negative_idx = (clf['polarity'] * X_test[:, clf['feature_index']] < clf['polarity'] * clf['threshold'])
+    predictions[negative_idx] = -1
+    y_pred += clf['alpha'] * predictions
 
+y_pred = np.sign(y_pred).flatten()
 
+acc = np.sum(y_test == y_pred) / len(y_test)
+print(acc)
